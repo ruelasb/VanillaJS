3b73316b7457a41ff4a9831d8cffc76bee614f3a
ruelasb <bruest@hotmail.com>
2017-03-07 12:34:09 -0800

    Initial commit: Zero exposure

---
+++ public/modular-ground-rules.md
@@ -0,0 +1,21 @@
+# Modular Javascript 
+---
+
+#### Javascript on the Object Literal Pattern 
+
+- Self-Contained module 
+  - Everything to do with my module is in my module.
+  - No global variables.
+  - If a module manages more than one thing it should be split up.
+
+- Separation of concerns
+
+- DRY code: Don't Repeat Yourself
+
+- Efficient DOM usage
+  - Very few $(selections)
+  
+- No memory leaks
+ - all events can be unbound
+
+by LearnCode.academy
---
+++ public/modularJS.js
@@ -0,0 +1,45 @@
+(function(){
+
+	var people = {
+		people: [],
+		init: function(){
+			this.cacheDom();
+			this.bindEvents();
+			this.render();
+		},
+		cacheDom: function(){
+			this.$el = $('#peopleModule');
+			this.$button = this.$el.find('button');
+			this.$input = this.$el.find('input');
+			this.$ul = this.$el.find('ul');
+			this.template = this.$el.find('#people-template').html();
+		},
+		bindEvents: function(){
+			this.$button.on('click', this.addPerson.bind(this))
+			this.$ul.on('click', 'a.delete', this.deletePerson.bind(this))
+		},
+		render: function(){
+			var data = {
+				people: this.people,
+			};
+			this.$ul.html(Mustache.render(this.template, data));
+		},
+		addPerson: function(){
+			const val = this.$input.val();
+			if (val === '') {return}; 
+			this.people.push(val)
+			this.render();
+			this.$input.val('');
+		},
+		deletePerson: function(e){
+			var $remove = $(e.target).closest('li');
+			var i = this.$ul.find('li').index($remove);
+
+			this.people.splice(i, 1);
+			this.render();
+		}
+	};
+
+	people.init();
+
+})()
